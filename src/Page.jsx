9dd7654443b50580251ca6cc95038384bb07fb16
crisxchan <chrisgonzales.online@gmail.com>
2022-07-27 14:22:47 +0800

    Aligned layout and refactor some components

--- src/Page.jsx
+++ src/Page.jsx
@@ -2,15 +2,16 @@ import Info from './components/Info/Info'
 import About from './components/About/About'
 import Interests from './components/Interests/Interests'
 import Footer from './components/Footer/Footer'
+import './page.css'
 
 function Page() {
   return (
-    <div>
+    <>
       <Info />
       <About />
       <Interests />
       <Footer />
-    </div>
+    </>
   )
 }
 
